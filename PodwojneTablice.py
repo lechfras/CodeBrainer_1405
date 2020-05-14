53115b542ac53d400e31cab0fa6a990ce1475840
arysteasz <arysteasz@gmail.com>
2020-05-14 20:20:42 +0200

    Tablice dwuwymiarowe

--- PodwojneTablice.py
+++ PodwojneTablice.py
@@ -1,4 +1,6 @@
+"""
 a = [[1, 2, 3], [4, 5, 6]]
+
 print(a[0])
 print(a[1])
 
@@ -8,7 +10,30 @@ print(b)
 b[2]=9
 print(b)
 
+print(len(a))
+
 for i in range(len(a)):
     for j in range(len(a[i])):
         print(a[i][j], end=' ')
-    print
+
+
+s =0
+for i in range(len(a)):
+    for j in range(len(a[i])):
+        s+=a[i][j]
+print(s)
+
+###Zadanie
+# mamy tablice n x m wypelnic "." "*"
+"""
+n,m =[int(i) for i in input().split()]
+a=[]
+for i in range(n):
+    a.append([])
+    for j in range(m):
+        if (i+j)%2==0:
+            a[i].append('.')
+        else:
+            a[i].append('*')
+for row in a:
+    print(' '.join(row))
